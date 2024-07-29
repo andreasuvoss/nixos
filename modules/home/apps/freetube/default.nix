f2d7c1322c5deef09ae94fa43768d4079d4ea765
Andreas Voss <andreas@anvo.dk>
2024-07-29 16:13:58 +0200

    Updated flake and removed freetube override fix

--- modules/home/apps/freetube/default.nix
+++ modules/home/apps/freetube/default.nix
@@ -1,8 +1,4 @@
 { pkgs, pkgs-unstable, lib, config, ... }:
-let
-  # I'm still getting the old version with this override, so I dont know what is going on
-  freetube-updated = pkgs.callPackage ./freetube-override.nix {  pkgs = pkgs-unstable; };
-in
 {
   options = {
     freetube.enable = lib.mkEnableOption "enable freetube";
@@ -10,7 +6,6 @@ in
   config = lib.mkIf config.freetube.enable {
     home.packages = [
       pkgs-unstable.freetube
-      # freetube-updated
     ];
   };
 }
--- modules/home/apps/freetube/freetube-override.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs , fetchurl }:
-pkgs.freetube.overrideAttrs rec {
-  version = "0.21.2";
-
-  src = fetchurl {
-    url = "https://github.com/FreeTubeApp/FreeTube/releases/download/v${version}-beta/freetube_${version}_amd64.AppImage";
-    sha256 = "sha256-Mk8qHDiUs2Nd8APMR8q1wZhTtxyzRhBAeXew9ogC3nk=";
-  };
-}
