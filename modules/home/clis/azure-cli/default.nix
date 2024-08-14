7f07fa36b5024acd19b254bd72ec48f345ea25b2
Andreas Voss <andreas@anvo.dk>
2024-08-14 07:34:52 +0000

    Rolled az cli version back as az login no longer works properly

--- modules/home/clis/azure-cli/default.nix
+++ modules/home/clis/azure-cli/default.nix
@@ -1,14 +1,19 @@
-{ pkgs-unstable, lib, config, ... }:
+{ pkgs, pkgs-unstable, lib, config, ... }:
 {
   options = {
     azure-cli.enable = lib.mkEnableOption "enable azure-cli";
   };
   config = lib.mkIf config.azure-cli.enable {
-    home.packages = with pkgs-unstable; [
+    home.packages = with pkgs; [
       (azure-cli.withExtensions [
-        azure-cli.extensions.application-insights 
         azure-cli.extensions.azure-devops
       ])
     ];
+    # home.packages = with pkgs-unstable; [
+    #   (azure-cli.withExtensions [
+    #     azure-cli.extensions.application-insights 
+    #     azure-cli.extensions.azure-devops
+    #   ])
+    # ];
   };
 }
