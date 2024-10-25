2eff68878565e791b8eac468975fbff8877a7965
Andreas Voss <andreas@anvo.dk>
2024-10-25 06:25:56 +0200

    made the hyprland workspace animation faster

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -128,12 +128,14 @@
         enabled = "yes";
         bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
         animation = [
+          #NAME,ONOFF,SPEED,CURVE,STYLE
           "windows, 1, 1, myBezier"
           "windowsOut, 1, 1, default, popin 80%"
           "border, 1, 10, default"
           "borderangle, 1, 8, default"
           "fade, 1, 1, default"
-          "workspaces, 1, 6, default"
+          # "workspaces, 1, 6, default"
+          "workspaces,1,1,default"
         ];
       };
       dwindle = {
