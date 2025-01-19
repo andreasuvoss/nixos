f8824e2d591c30b87a3f53e061bee4ab355341b3
Andreas Voss <andreas@anvo.dk>
2025-01-19 21:20:09 +0100

    Updated flake and hyprland layout

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, config, ...}:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   options = {
     hyprland.enable = lib.mkEnableOption "enable hyprland";
@@ -6,7 +11,7 @@
       default = false;
     };
     hyprland.startTeams = lib.mkEnableOption "starts teams automatically";
-    hyprland.enableKanshi = lib.mkEnableOption "starts kanshi"; 
+    hyprland.enableKanshi = lib.mkEnableOption "starts kanshi";
     hyprland.wlogout.command = lib.mkOption {
       default = "wlogout -b 6 -s -R 1500 -L 1500 -T 600 -B 600";
       description = "command to run when opening wlogout";
@@ -54,8 +59,8 @@
         text-color = "#f8f8f2";
         input-color = "#ff79c6";
         clip-to-padding = false;
-        
-        default-result-background= "#282a36";
+
+        default-result-background = "#282a36";
         default-result-background-padding = "4, -1";
         result-spacing = 8;
         selection-color = "#50fa7b";
@@ -74,38 +79,41 @@
       "$menu" = "tofi-drun | xargs hyprctl dispatch exec --";
       "$lock" = "swaylock";
       "$wlogoutCmd" = config.hyprland.wlogout.command;
-      exec-once = [
-        "swaybg --color 000000" # at some point maybe look into swww
-        "waybar"
-        "sleep 1; swaync"
-        "sleep 1; exec ${pkgs.lxqt.lxqt-policykit}/bin/lxqt-policykit-agent"
-        "discord --start-minimized"
-        "sleep 1; signal-desktop"
-        "steam %U -nochatui -nofriendsui -silent"
-        "exec swayidle -w timeout 180 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' timeout 300 '$lock' before-sleep '$lock'"
-        "sleep 1; megasync"
-        "sleep 1; bitwarden"
-        # The command below might work for keeping xclip and wl-clipboard in sync, I had some issues copying text into proton games
-        # "wl-paste -t text -w bash -c '[ \"$(xclip -selection clipboard -o)\" = \"$(wl-paste -n)\" ] || [ \"$(wl-paste -l | grep image)\" = \"\" ] && xclip -selection clipboard'"
-      ] 
-      ++ lib.optional config.hyprland.startTeams "sleep 1; teams --minimized true"
-      # ++ lib.optional config._1password.enable "sleep 1; 1password --silent"
-      ++ lib.optional config.hyprland.enableKanshi "exec ${pkgs.kanshi}/bin/kanshi"
-      ++ lib.optional (!config.swayidle.enableWorkaround) "exec ${pkgs.sway-audio-idle-inhibit}/bin/sway-audio-idle-inhibit";
+      exec-once =
+        [
+          "swaybg --color 000000" # at some point maybe look into swww
+          "waybar"
+          "sleep 1; swaync"
+          "sleep 1; exec ${pkgs.lxqt.lxqt-policykit}/bin/lxqt-policykit-agent"
+          "discord --start-minimized"
+          "sleep 1; signal-desktop"
+          "steam %U -nochatui -nofriendsui -silent"
+          "exec swayidle -w timeout 180 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' timeout 300 '$lock' before-sleep '$lock'"
+          "sleep 1; megasync"
+          "sleep 1; bitwarden"
+          # The command below might work for keeping xclip and wl-clipboard in sync, I had some issues copying text into proton games
+          # "wl-paste -t text -w bash -c '[ \"$(xclip -selection clipboard -o)\" = \"$(wl-paste -n)\" ] || [ \"$(wl-paste -l | grep image)\" = \"\" ] && xclip -selection clipboard'"
+        ]
+        ++ lib.optional config.hyprland.startTeams "sleep 1; teams --minimized true"
+        # ++ lib.optional config._1password.enable "sleep 1; 1password --silent"
+        ++ lib.optional config.hyprland.enableKanshi "exec ${pkgs.kanshi}/bin/kanshi"
+        ++ lib.optional (
+          !config.swayidle.enableWorkaround
+        ) "exec ${pkgs.sway-audio-idle-inhibit}/bin/sway-audio-idle-inhibit";
       env = [
         # "XDG_CURRENT_DESKTOP,sway"
         "XCURSOR_SIZE,24"
         "QT_QPA_PLATFORMTHEME,qt5ct"
       ];
       input = {
-          kb_layout = "us";
-          kb_variant = "altgr-intl";
-          follow_mouse = 1;
-          sensitivity = -0.5;
-          accel_profile = "flat";
-          force_no_accel = true;
-          touchpad.natural_scroll = config.hyprland.natural_scroll;
-          touchpad.clickfinger_behavior = config.hyprland.natural_scroll;
+        kb_layout = "us";
+        kb_variant = "altgr-intl";
+        follow_mouse = 1;
+        sensitivity = -0.5;
+        accel_profile = "flat";
+        force_no_accel = true;
+        touchpad.natural_scroll = config.hyprland.natural_scroll;
+        touchpad.clickfinger_behavior = config.hyprland.natural_scroll;
       };
       general = {
         gaps_in = 2;
@@ -113,8 +121,16 @@
         border_size = 1;
         "col.active_border" = "rgb(bd93f9)";
         "col.inactive_border" = "rgba(595959aa)";
-        layout = "dwindle";
+        # layout = "dwindle";
+        layout = "master";
         allow_tearing = false;
+      };
+      master = {
+        allow_small_split = true;
+        # orientation = "left"; # to use entire monitor
+        orientation = "center";
+        always_center_master = true;
+        mfact = "0.55";
       };
       cursor = {
         inactive_timeout = 3;
@@ -213,51 +229,69 @@
         "suppressevent maximize, class:.*"
       ];
 
-      bind = [
-        "$mainMod, Q, exec, $terminal"
-        "$mainMod, M, exec, sleep 0.1; hyprctl dispatch dpms off; $lock"
-        "$mainMod, ESCAPE, exec, pgrep -U $USER wlogout >/dev/null || exec $wlogoutCmd" # I wonder how this looks on a smaller display..
-        # "$mainMod, E, exec, $fileManager"
-        "$mainMod, V, togglefloating"
-        "$mainMod, C, togglefloating"
-        "$mainMod, X, killactive"
-        "$mainMod, C, centerwindow"
-        "$mainMod, N, exec, swaync-client -t -sw"
-        "ALT, SPACE, exec, $menu"
+      bind =
+        [
+          "$mainMod, Q, exec, $terminal"
+          "$mainMod, M, exec, sleep 0.1; hyprctl dispatch dpms off; $lock"
+          "$mainMod, ESCAPE, exec, pgrep -U $USER wlogout >/dev/null || exec $wlogoutCmd" # I wonder how this looks on a smaller display..
+          # "$mainMod, E, exec, $fileManager"
+          "$mainMod, V, togglefloating"
+          "$mainMod, C, togglefloating"
+          "$mainMod, X, killactive"
+          "$mainMod, C, centerwindow"
+          "$mainMod, N, exec, swaync-client -t -sw"
+          "ALT, SPACE, exec, $menu"
 
-        "$mainMod, H, movefocus, l"
-        "$mainMod, L, movefocus, r"
-        "$mainMod, K, movefocus, u"
-        "$mainMod, J, movefocus, d"
-        "$mainMod SHIFT, H, movewindow, l"
-        "$mainMod SHIFT, L, movewindow, r"
-        "$mainMod SHIFT, K, movewindow, u"
-        "$mainMod SHIFT, J, movewindow, d"
-        "$mainMod CTRL, H, resizeactive, 100 0"
-        "$mainMod CTRL, L, resizeactive, -100 0"
-        "$mainMod CTRL, K, resizeactive, 0 -100"
-        "$mainMod CTRL, J, resizeactive, 0 100"
+          "$mainMod, H, movefocus, l"
+          "$mainMod, L, movefocus, r"
+          "$mainMod, K, movefocus, u"
+          "$mainMod, J, movefocus, d"
+          "$mainMod SHIFT, H, movewindow, l"
+          "$mainMod SHIFT, L, movewindow, r"
+          "$mainMod SHIFT, K, movewindow, u"
+          "$mainMod SHIFT, J, movewindow, d"
+          "$mainMod CTRL, H, resizeactive, 100 0"
+          "$mainMod CTRL, L, resizeactive, -100 0"
+          "$mainMod CTRL, K, resizeactive, 0 -100"
+          "$mainMod CTRL, J, resizeactive, 0 100"
 
-        ", PRINT, exec, grim -g \"$(slurp)\" - | convert - -shave 1x1 PNG:- | swappy -f -"
+          # Layout config
+          "$mainMod CTRL, comma, layoutmsg, addmaster"
+          "$mainMod CTRL, period, layoutmsg, removemaster"
+          "$mainMod SHIFT, down, layoutmsg, orientationcenter"
+          "$mainMod SHIFT, left, layoutmsg, orientationleft"
+          "$mainMod SHIFT, right, layoutmsg, orientationright"
+          "$mainMod SHIFT, RETURN, layoutmsg, swapwithmaster"
+          "$mainMod SHIFT, period, layoutmsg, mfact +0.1"
+          "$mainMod SHIFT, comma, layoutmsg, mfact -0.1"
+          "$mainMod SHIFT, F, fullscreen"
+          "$mainMod ALT, F, togglefloating"
+          "$mainMod ALT, C, centerwindow"
 
-        "$mainMod, S, togglespecialworkspace, magic"
-        "$mainMod SHIFT, S, movetoworkspace, special:magic"
+          ", PRINT, exec, grim -g \"$(slurp)\" - | convert - -shave 1x1 PNG:- | swappy -f -"
 
-        "$mainMod, mouse_down, workspace, e+1"
-        "$mainMod, mouse_up, workspace, e-1"
-      ]++ (
-        builtins.concatLists (builtins.genList (
-        x: let
-          ws = let
-            c = (x + 1) / 10;
-          in
-            builtins.toString (x + 1 - (c * 10));
-          in [
-            "$mainMod, ${ws}, workspace, ${toString (x + 1)}"
-            "$mainMod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-          ]
-      )
-      10));
+          "$mainMod, S, togglespecialworkspace, magic"
+          "$mainMod SHIFT, S, movetoworkspace, special:magic"
+
+          "$mainMod, mouse_down, workspace, e+1"
+          "$mainMod, mouse_up, workspace, e-1"
+        ]
+        ++ (builtins.concatLists (
+          builtins.genList (
+            x:
+            let
+              ws =
+                let
+                  c = (x + 1) / 10;
+                in
+                builtins.toString (x + 1 - (c * 10));
+            in
+            [
+              "$mainMod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mainMod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]
+          ) 10
+        ));
       bindm = [
         "$mainMod, mouse:272, movewindow"
         "$mainMod, mouse:273, resizewindow"
