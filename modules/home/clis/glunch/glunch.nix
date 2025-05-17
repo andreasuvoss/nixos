9ea29231b48821df3a1d07b2ce5caa13252e96d0
Andreas Voss <andreas@anvo.dk>
2025-05-17 17:49:06 +0200

    chore: updated glunch

--- modules/home/clis/glunch/glunch.nix
+++ modules/home/clis/glunch/glunch.nix
@@ -3,18 +3,21 @@
   buildGoModule,
   fetchFromGitHub,
 }:
+let
+  version = "0.0.2";
+in
 buildGoModule {
   pname = "glunch";
-  version = "0.0.1";
+  version = version;
 
   src = fetchFromGitHub {
     owner = "andreasuvoss";
     repo = "glunch";
-    rev = "v0.0.1";
-    hash = "sha256-8xBNkmNtRsYuQ3eY7/Z4x/e44+Sfg4YgbJucJryk+FU=";
+    rev = "v${version}";
+    hash = "sha256-Djs/yXqDWKM7iSQdNAOkwykzNQ7ImrrENeGrSUxNd8U=";
   };
 
-  vendorHash = "sha256-m5mBubfbXXqXKsygF5j7cHEY+bXhAMcXUts5KBKoLzM=";
+  vendorHash = null;
 
   meta = {
     description = "Get the lunch menu at work";
