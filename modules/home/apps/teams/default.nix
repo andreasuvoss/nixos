e445ff6a89d0df6fefd9b0a0b3777f04b82248af
Andreas Voss <andreas@anvo.dk>
2025-10-23 07:09:25 +0200

    feat: desktop entry for teams

--- modules/home/apps/teams/default.nix
+++ modules/home/apps/teams/default.nix
@@ -4,6 +4,14 @@
     teams.enable = lib.mkEnableOption "enable teams" ;
   };
   config = lib.mkIf config.teams.enable {
+    xdg.desktopEntries.teams = {
+      name = "Microsoft Teams";
+      genericName = "";
+      exec = "teams";
+      terminal = false;
+      categories = [ "Application" ];
+    };
+
     home.file.".config/firejail/teams-for-linux.local" = {
       enable = true;
       source = ./teams-for-linux.local;
