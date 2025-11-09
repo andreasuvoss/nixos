b9b8db72d03c126532d18cb86f3aabefeb56fa87
Andreas Voss <andreas@anvo.dk>
2025-11-09 13:07:24 +0100

    fix: fixed the borders around floating windows

--- modules/home/clis/nvim/plugins/lsp.lua
+++ modules/home/clis/nvim/plugins/lsp.lua
@@ -20,13 +20,7 @@ local on_attach = function(_, bufnr)
         },
     })
 
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-        border = "single",
-    })
-
-    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-        border = "single",
-    })
+    vim.o.winborder = 'single'
 
     map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
     map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
