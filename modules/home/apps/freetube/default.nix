25e8a7bbee6a15c3ff61d789511e0b7dc2a256f8
Andreas Voss <andreas@anvo.dk>
2026-06-02 15:11:05 +0200

    cleanup

--- modules/home/apps/freetube/default.nix
+++ modules/home/apps/freetube/default.nix
@@ -4,8 +4,20 @@
     freetube.enable = lib.mkEnableOption "enable freetube";
   };
   config = lib.mkIf config.freetube.enable {
-    home.packages = [
-      pkgs-unstable.freetube
-    ];
+    programs.freetube = {
+      enable = true;
+      package = pkgs-unstable.freetube.overrideAttrs(old: {
+        patches = (old.patches or []) ++ [
+          (pkgs.fetchpatch {
+            url = "github.com/FreeTubeApp/FreeTube/commit/cda95536d170310c59393dcf6024b818f8e08e5e.patch";
+            hash = "sha256-ppfFjNH1DoiTq+xGmqQwJraYIh6UEsAtlSfNBO1O2AM=";
+          })
+        ];
+      });
+
+    };
+    # home.packages = [
+    #   pkgs-unstable.freetube
+    # ];
   };
 }
